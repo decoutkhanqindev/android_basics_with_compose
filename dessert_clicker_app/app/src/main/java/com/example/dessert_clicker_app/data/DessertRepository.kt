9d3fac3769ecfd1e907328cc0a35c4df2e589ec4
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-27 00:18:57 +0700

    update DessertRepository with other initial data

--- dessert_clicker_app/app/src/main/java/com/example/dessert_clicker_app/data/DessertRepository.kt
+++ dessert_clicker_app/app/src/main/java/com/example/dessert_clicker_app/data/DessertRepository.kt
@@ -5,18 +5,18 @@ import com.example.dessert_clicker_app.model.Dessert
 
 object DessertRepository {
   val desserts: List<Dessert> = listOf(
-    Dessert(R.drawable.cupcake, 5, 5),
-    Dessert(R.drawable.donut, 10, 10),
-    Dessert(R.drawable.eclair, 15, 5),
-    Dessert(R.drawable.froyo, 30, 2),
-    Dessert(R.drawable.gingerbread, 50, 5),
-    Dessert(R.drawable.honeycomb, 100, 8),
-    Dessert(R.drawable.icecreamsandwich, 500, 5),
-    Dessert(R.drawable.jellybean, 1000, 1),
-    Dessert(R.drawable.kitkat, 2000, 2),
-    Dessert(R.drawable.lollipop, 3000, 8),
-    Dessert(R.drawable.marshmallow, 4000, 2),
-    Dessert(R.drawable.nougat, 5000, 9),
-    Dessert(R.drawable.oreo, 6000, 10)
+    Dessert(R.drawable.cupcake, 5, 0),
+    Dessert(R.drawable.donut, 10, 5),
+    Dessert(R.drawable.eclair, 15, 20),
+    Dessert(R.drawable.froyo, 30, 50),
+    Dessert(R.drawable.gingerbread, 50, 100),
+    Dessert(R.drawable.honeycomb, 100, 200),
+    Dessert(R.drawable.icecreamsandwich, 500, 500),
+    Dessert(R.drawable.jellybean, 1000, 1000),
+    Dessert(R.drawable.kitkat, 2000, 2000),
+    Dessert(R.drawable.lollipop, 3000, 4000),
+    Dessert(R.drawable.marshmallow, 4000, 8000),
+    Dessert(R.drawable.nougat, 5000, 16000),
+    Dessert(R.drawable.oreo, 6000, 20000)
   )
 }
