9d970aa7e66202c23970e54471f1ef2d8eb9af64
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-26 13:21:07 +0700

    update custom theming

--- dessert_clicker_app/app/src/main/java/com/example/dessert_clicker_app/ui/theme/Theme.kt
+++ dessert_clicker_app/app/src/main/java/com/example/dessert_clicker_app/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.dessert_clicker_app.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,35 +9,34 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-  primary = Purple80,
-  secondary = PurpleGrey80,
-  tertiary = Pink80
+  primary = md_theme_dark_primary,
+  onPrimary = md_theme_dark_onPrimary,
+  secondaryContainer = md_theme_dark_secondaryContainer,
+  onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+  background = md_theme_dark_background,
 )
 
 private val LightColorScheme = lightColorScheme(
-  primary = Purple40,
-  secondary = PurpleGrey40,
-  tertiary = Pink40
-
-  /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+  primary = md_theme_light_primary,
+  onPrimary = md_theme_light_onPrimary,
+  secondaryContainer = md_theme_light_secondaryContainer,
+  onSecondaryContainer = md_theme_light_onSecondaryContainer,
+  background = md_theme_light_background,
 )
 
 @Composable
-fun Dessert_clicker_appTheme(
+fun DessertClickerAppTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
+  // Dynamic color in this app is turned off for learning purposes
+  dynamicColor: Boolean = false,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {
@@ -48,10 +48,18 @@ fun Dessert_clicker_appTheme(
     darkTheme -> DarkColorScheme
     else -> LightColorScheme
   }
+  val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      window.statusBarColor = colorScheme.primary.toArgb()
+      window.navigationBarColor = colorScheme.secondaryContainer.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+    }
+  }
 
   MaterialTheme(
     colorScheme = colorScheme,
-    typography = Typography,
     content = content
   )
 }
