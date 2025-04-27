78d999283ec3eccb4b609be8cb5edca215a45493
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-28 00:02:59 +0700

    add MenuDataSource class to provide initial menu data for entrees, side dishes, and accompaniments

---
+++ lunch_tray_app/app/src/main/java/com/example/lunch_tray_app/data/MenuDataSource.kt
@@ -0,0 +1,73 @@
+package com.example.lunch_tray_app.data
+
+import com.example.lunch_tray_app.model.MenuItem.Accompaniment
+import com.example.lunch_tray_app.model.MenuItem.Entree
+import com.example.lunch_tray_app.model.MenuItem.SideDish
+
+object MenuDataSource {
+  val entreesMenu = listOf(
+    Entree(
+      name = "Cauliflower",
+      description = "Whole cauliflower, brined, roasted, and deep fried",
+      price = 7.00,
+    ),
+    Entree(
+      name = "Three Bean Chili",
+      description = "Black beans, red beans, kidney beans, slow cooked, topped with onion",
+      price = 4.00,
+    ),
+    Entree(
+      name = "Mushroom Pasta",
+      description = "Penne pasta, mushrooms, basil, with plum tomatoes cooked in garlic " +
+          "and olive oil",
+      price = 5.50,
+    ),
+    Entree(
+      name = "Spicy Black Bean Skillet",
+      description = "Seasonal vegetables, black beans, house spice blend, served with " +
+          "avocado and quick pickled onions",
+      price = 5.50,
+    )
+  )
+
+  val sideDishesMenu = listOf(
+    SideDish(
+      name = "Summer Salad",
+      description = "Heirloom tomatoes, butter lettuce, peaches, avocado, balsamic dressing",
+      price = 2.50,
+    ),
+    SideDish(
+      name = "Butternut Squash Soup",
+      description = "Roasted butternut squash, roasted peppers, chili oil",
+      price = 3.00,
+    ),
+    SideDish(
+      name = "Spicy Potatoes",
+      description = "Marble potatoes, roasted, and fried in house spice blend",
+      price = 2.00,
+    ),
+    SideDish(
+      name = "Coconut Rice",
+      description = "Rice, coconut milk, lime, and sugar",
+      price = 1.50,
+    )
+  )
+
+  val accompanimentsMenu = listOf(
+    Accompaniment(
+      name = "Lunch Roll",
+      description = "Fresh baked roll made in house",
+      price = 0.50,
+    ),
+    Accompaniment(
+      name = "Mixed Berries",
+      description = "Strawberries, blueberries, raspberries, and huckleberries",
+      price = 1.00,
+    ),
+    Accompaniment(
+      name = "Pickled Veggies",
+      description = "Pickled cucumbers and carrots, made in house",
+      price = 0.50,
+    )
+  )
+}
