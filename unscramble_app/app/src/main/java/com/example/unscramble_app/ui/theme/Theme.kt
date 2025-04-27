1e9c0cafc3843a5409e0b7c8475337a0c1e7b6d9
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-27 15:17:04 +0700

    update theming configs

--- unscramble_app/app/src/main/java/com/example/unscramble_app/ui/theme/Theme.kt
+++ unscramble_app/app/src/main/java/com/example/unscramble_app/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Unscramble_appTheme(
+fun UnscrambleAppTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
   dynamicColor: Boolean = true,
