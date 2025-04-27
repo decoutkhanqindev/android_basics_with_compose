2f7883580a16f9d9a937f39646ff7f6ac14b340e
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-27 22:59:13 +0700

    update MainActivity to use cupcake app

--- cupcake_app/app/src/main/java/com/example/cupcake_app/MainActivity.kt
+++ cupcake_app/app/src/main/java/com/example/cupcake_app/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcake_app.ui.screens.CupcakeApp
 import com.example.cupcake_app.ui.theme.Cupcake_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,8 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       Cupcake_appTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+        CupcakeApp(modifier = Modifier.fillMaxSize())
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  Cupcake_appTheme {
-    Greeting("Android")
   }
 }
