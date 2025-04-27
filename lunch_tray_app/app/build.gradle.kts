0546527517d09d42458b6b25513f9d5b68512e34
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-28 00:01:53 +0700

    add dependencies

--- lunch_tray_app/app/build.gradle.kts
+++ lunch_tray_app/app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
   androidTestImplementation(libs.androidx.ui.test.junit4)
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
+
+  // Navigation
+  implementation("androidx.navigation:navigation-compose:2.8.9")
+
+  // ViewModel
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.9")
 }
