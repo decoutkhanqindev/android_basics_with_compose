2b495d30f6cbf28892d4f72f1885dca69da92a9d
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-02 00:35:33 +0700

    Add ReplyNavigationType to ReplyApp to handle change ui with different screen

--- reply_app/app/src/main/java/com/example/reply_app/ui/screens/ReplyApp.kt
+++ reply_app/app/src/main/java/com/example/reply_app/ui/screens/ReplyApp.kt
@@ -1,18 +1,42 @@
 package com.example.reply_app.ui.screens
 
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply_app.ui.theme.ReplyAppTheme
+import com.example.reply_app.ui.utils.ReplyNavigationType
 
 @Composable
-fun ReplyApp(modifier: Modifier = Modifier) {
+fun ReplyApp(
+  windowSize: WindowWidthSizeClass,
+  modifier: Modifier = Modifier
+) {
   val replyViewModel: ReplyViewModel = viewModel()
   val replyUiState = replyViewModel.uiState.collectAsState().value
 
+  val navigationType = when (windowSize) {
+    WindowWidthSizeClass.Compact -> {
+      ReplyNavigationType.BOTTOM_NAVIGATION
+    }
+
+    WindowWidthSizeClass.Medium -> {
+      ReplyNavigationType.NAVIGATION_RAIL
+    }
+
+    WindowWidthSizeClass.Expanded -> {
+      ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+    }
+
+    else -> {
+      ReplyNavigationType.BOTTOM_NAVIGATION
+    }
+  }
+
   ReplyHomeScreen(
+    navigationType = navigationType,
     replyUiState = replyUiState,
     onTabPressed = {
       replyViewModel.updateCurrentMailbox(mailboxType = it)
@@ -32,6 +56,6 @@ fun ReplyApp(modifier: Modifier = Modifier) {
 @Composable
 fun ReplyAppPreview() {
   ReplyAppTheme {
-    ReplyApp()
+    ReplyApp(windowSize = WindowWidthSizeClass.Compact)
   }
 }
