008946c391fe8df9abd1c0a11f5bf2eb18f2e95d
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-01 23:42:56 +0700

    add ReplyViewModel for state management and update data

---
+++ reply_app/app/src/main/java/com/example/reply_app/ui/screens/ReplyViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.reply_app.ui.screens
+
+import androidx.lifecycle.ViewModel
+import com.example.reply_app.data.EmailDataProvider
+import com.example.reply_app.model.Email
+import com.example.reply_app.model.MailboxType
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class ReplyViewModel : ViewModel() {
+  private val _uiState = MutableStateFlow(ReplyUiState())
+  val uiState = _uiState.asStateFlow()
+
+  init {
+    initializeUIState()
+  }
+
+  private fun initializeUIState() {
+    val mailboxes: Map<MailboxType, List<Email>> =
+      EmailDataProvider.allEmails.groupBy { it.mailbox }
+    _uiState.value =
+      ReplyUiState(
+        mailboxes = mailboxes,
+        currentSelectedEmail = mailboxes[MailboxType.Inbox]?.get(0)
+          ?: EmailDataProvider.defaultEmail
+      )
+  }
+
+  fun updateDetailsScreenStates(email: Email) {
+    _uiState.update { currentState ->
+      currentState.copy(
+        currentSelectedEmail = email,
+        isShowingHomepage = false
+      )
+    }
+  }
+
+  fun resetHomeScreenStates() {
+    _uiState.update { currentState ->
+      currentState.copy(
+        currentSelectedEmail = currentState.mailboxes[currentState.currentMailbox]?.get(0)
+          ?: EmailDataProvider.defaultEmail,
+        isShowingHomepage = true
+      )
+    }
+  }
+
+  fun updateCurrentMailbox(mailboxType: MailboxType) {
+    _uiState.update { currentState ->
+      currentState.copy(
+        currentMailbox = mailboxType
+      )
+    }
+  }
+}
