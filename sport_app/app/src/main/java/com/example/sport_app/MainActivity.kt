828f3c6a19daf4d7ff5981f4452bd5c77bb6dca4
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-02 17:05:06 +0700

    add sports app screen for compact and expanded screen sizes

--- sport_app/app/src/main/java/com/example/sport_app/MainActivity.kt
+++ sport_app/app/src/main/java/com/example/sport_app/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sport_app.ui.theme.Sport_appTheme
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import com.example.sport_app.ui.SportsApp
+import com.example.sport_app.ui.theme.SportsAppTheme
 
 class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      Sport_appTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+      SportsAppTheme {
+        val windowSize = calculateWindowSizeClass(this)
+        SportsApp(windowSize.widthSizeClass)
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  Sport_appTheme {
-    Greeting("Android")
   }
 }
