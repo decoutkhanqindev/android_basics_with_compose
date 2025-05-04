929d68bbdc05717cfbe6a62e7b2c2899804c38a1
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-05 01:03:25 +0700

    update main activity to use app route

--- run_tracker_app/app/src/main/java/com/example/run_tracker_app/MainActivity.kt
+++ run_tracker_app/app/src/main/java/com/example/run_tracker_app/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.run_tracker_app.ui.RunTrackerApp
 import com.example.run_tracker_app.ui.theme.Run_tracker_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,16 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       Run_tracker_appTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+        RunTrackerApp()
       }
     }
   }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RunTrackerAppPreview() {
   Run_tracker_appTheme {
-    Greeting("Android")
+    RunTrackerApp()
   }
 }
