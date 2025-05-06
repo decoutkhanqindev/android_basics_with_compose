b67aab44ef52741f1855b16bbe305efb0297ed70
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-06 23:08:12 +0700

    add BookshelfApp to MainActivity

--- bookshelf_app/app/src/main/java/com/example/bookshelf_app/MainActivity.kt
+++ bookshelf_app/app/src/main/java/com/example/bookshelf_app/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookshelf_app.ui.BookshelfApp
 import com.example.bookshelf_app.ui.theme.Bookshelf_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       Bookshelf_appTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+        BookshelfApp()
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  Bookshelf_appTheme {
-    Greeting("Android")
   }
 }
