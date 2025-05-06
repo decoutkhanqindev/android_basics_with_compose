5dbe733e3cbcb48119a875f0da36d22a073f45e7
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-07 00:26:26 +0700

    update BookDetailsScreen ui

--- bookshelf_app/app/src/main/java/com/example/bookshelf_app/ui/screens/details/BookDetailsScreen.kt
+++ bookshelf_app/app/src/main/java/com/example/bookshelf_app/ui/screens/details/BookDetailsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -12,6 +13,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -70,8 +72,9 @@ private fun BookDetails(
       placeholder = painterResource(id = R.drawable.loading_img),
       contentScale = ContentScale.FillBounds,
       modifier = Modifier
-        .aspectRatio(.6f)
-        .padding(8.dp)
+        .fillMaxWidth()
+        .aspectRatio(1f)
+        .align(Alignment.CenterHorizontally)
     )
     Column {
       Text(
