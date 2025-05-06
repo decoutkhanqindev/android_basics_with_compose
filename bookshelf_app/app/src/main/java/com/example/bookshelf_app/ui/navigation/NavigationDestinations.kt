631c91e38790349b39b90ba7012f70cac67c86bb
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-07 00:28:46 +0700

    change NavigationDestinations to sealed interface for type safe navigation

--- bookshelf_app/app/src/main/java/com/example/bookshelf_app/ui/navigation/NavigationDestinations.kt
+++ bookshelf_app/app/src/main/java/com/example/bookshelf_app/ui/navigation/NavigationDestinations.kt
@@ -1,5 +1,11 @@
 package com.example.bookshelf_app.ui.navigation
 
-enum class NavigationDestinations {
-  SEARCH, DETAILS
+import kotlinx.serialization.Serializable
+
+sealed interface NavigationDestinations {
+  @Serializable
+  object SearchBooksDestination : NavigationDestinations
+
+  @Serializable
+  data class BooksDetailsDestination(val id: String?) : NavigationDestinations
 }
