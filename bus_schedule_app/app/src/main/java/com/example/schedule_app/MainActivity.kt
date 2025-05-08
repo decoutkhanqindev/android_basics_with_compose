8f875fcf21680725b966201070b7cfa864b85d56
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-09 01:36:09 +0700

    implement MainActivity to use app root

--- bus_schedule_app/app/src/main/java/com/example/schedule_app/MainActivity.kt
+++ bus_schedule_app/app/src/main/java/com/example/schedule_app/MainActivity.kt
@@ -4,44 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.schedule_app.ui.theme.Schedule_appTheme
+import com.example.schedule_app.ui.BusScheduleApp
+import com.example.schedule_app.ui.theme.BusScheduleAppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      Schedule_appTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
+      BusScheduleAppTheme {
+        BusScheduleApp()
       }
     }
   }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  Schedule_appTheme {
-    Greeting("Android")
-  }
-}
