c037c15e26ba23437a14917d2b57ccbb93350702
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-12 02:00:20 +0700

    add Airport and Favorite represent single row in their respective tables

---
+++ flight_search_app/app/src/main/java/com/example/flight_search_app/data/local/database/entity/Airport.kt
@@ -0,0 +1,17 @@
+package com.example.flight_search_app.data.local.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "airport")
+data class Airport(
+  @PrimaryKey(autoGenerate = true)
+  val id: Int = 0,
+  @ColumnInfo(name = "iata_code")
+  val iataCode: String,
+  @ColumnInfo(name = "name")
+  val name: String,
+  @ColumnInfo(name = "passengers")
+  val passengers: Int
+)
